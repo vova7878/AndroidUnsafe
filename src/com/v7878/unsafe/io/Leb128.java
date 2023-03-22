6f6e40240db9b60d99ddaee27965ed5e8b26d653
vova7878 <46866705+vova7878@users.noreply.github.com>
2023-03-22 23:07:36 +0700

    delete unused code

--- src/com/v7878/unsafe/io/Leb128.java
+++ src/com/v7878/unsafe/io/Leb128.java
@@ -5,32 +5,6 @@ public class Leb128 {
     private Leb128() {
     }
 
-    /*public static int unsignedLeb128Size(int value) {
-        // TODO: This could be much cleverer.
-        int remaining = value >> 7;
-        int count = 0;
-        while (remaining != 0) {
-            remaining >>= 7;
-            count++;
-        }
-        return count + 1;
-    }
-
-    public static int signedLeb128Size(int value) {
-        // TODO: This could be much cleverer.
-        int remaining = value >> 7;
-        int count = 0;
-        boolean hasMore = true;
-        int end = ((value & Integer.MIN_VALUE) == 0) ? 0 : -1;
-        while (hasMore) {
-            hasMore = (remaining != end)
-                    || ((remaining & 1) != ((value >> 6) & 1));
-            value = remaining;
-            remaining >>= 7;
-            count++;
-        }
-        return count;
-    }*/
     public static int readSignedLeb128(RandomInput in) {
         int result = 0;
         int cur;

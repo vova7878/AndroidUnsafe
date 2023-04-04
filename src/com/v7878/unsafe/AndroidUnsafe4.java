be01b03e60446d44c7107812158661484639f181
vova7878 <46866705+vova7878@users.noreply.github.com>
2023-04-04 12:34:30 +0700

    rename Unsafe to SunUnsafe

--- src/com/v7878/unsafe/AndroidUnsafe4.java
+++ src/com/v7878/unsafe/AndroidUnsafe4.java
@@ -291,19 +291,19 @@ public class AndroidUnsafe4 extends AndroidUnsafe3 {
 
     @DangerLevel(DangerLevel.VERY_CAREFUL)
     public static void putObjectRaw(long address, Object value) {
-        Unsafe.putInt(address, rawObjectToInt(value));
+        putIntN(address, rawObjectToInt(value));
     }
 
-    @DangerLevel(DangerLevel.MAX)
+    @DangerLevel(DangerLevel.VERY_CAREFUL)
     public static Object rawIntToObject(int obj) {
         int[] arr = new int[1];
         arr[0] = obj;
         return getObject(arr, ARRAY_INT_BASE_OFFSET);
     }
 
-    @DangerLevel(DangerLevel.MAX)
+    @DangerLevel(DangerLevel.VERY_CAREFUL)
     public static Object getObjectRaw(long address) {
-        return rawIntToObject(Unsafe.getInt(address));
+        return rawIntToObject(getIntN(address));
     }
 
     private static Boolean kPoisonReferences;

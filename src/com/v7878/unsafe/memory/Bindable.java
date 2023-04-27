96e3cee2ce4685f53880513233eaaafb8c3e2df4
vova7878 <46866705+vova7878@users.noreply.github.com>
2023-04-27 18:22:02 +0700

    add Bindable.CSTRING

--- src/com/v7878/unsafe/memory/Bindable.java
+++ src/com/v7878/unsafe/memory/Bindable.java
@@ -1,6 +1,9 @@
 package com.v7878.unsafe.memory;
 
+@FunctionalInterface
 public interface Bindable<T> {
 
-    public T bind(Addressable p);
+    public static final Bindable<String> CSTRING = a -> a.pointer().getCString();
+
+    public T bind(Addressable a);
 }

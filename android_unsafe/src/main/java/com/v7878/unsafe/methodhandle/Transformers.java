218cf81d160c554106fdf643e7a496984c3a0962
v7878 <46866705+vova7878@users.noreply.github.com>
2023-06-15 14:27:26 +0700

    rewrite Transformers

--- android_unsafe/src/main/java/com/v7878/unsafe/methodhandle/Transformers.java
+++ android_unsafe/src/main/java/com/v7878/unsafe/methodhandle/Transformers.java
@@ -1,5 +1,6 @@
 package com.v7878.unsafe.methodhandle;
 
+import static com.v7878.unsafe.AndroidUnsafe3.ClassMirror;
 import static com.v7878.unsafe.AndroidUnsafe3.arrayCast;
 import static com.v7878.unsafe.AndroidUnsafe3.unreflectDirect;
 import static com.v7878.unsafe.AndroidUnsafe5.allocateInstance;
@@ -45,15 +46,21 @@ import java.lang.invoke.WrongMethodTypeException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Objects;
 
 import dalvik.system.DexFile;
+
+@Keep
+class VariadicType {
+    private VariadicType() {
+    }
+}
 
 public class Transformers {
 
     @SuppressWarnings("unchecked")
     private static final Class<MethodHandle> invoke_transformer = (Class<MethodHandle>) nothrows_run(
             () -> Class.forName("java.lang.invoke.Transformers$Transformer"));
-    private static final Class<MethodHandle> transformer;
     private static final Constructor<MethodHandle> transformer_constructor;
     private static final MethodHandle directAsType = nothrows_run(() -> unreflectDirect(
             getDeclaredMethod(MethodHandle.class, "asType", MethodType.class)));
@@ -298,39 +305,63 @@ public class Transformers {
         setTrusted(dex);
 
         ClassLoader loader = Transformers.class.getClassLoader();
-        //noinspection unchecked
-        transformer = (Class<MethodHandle>) loadClass(dex, transformer_name, loader);
         Class<?> invoker_class = loadClass(dex, invoker_name, loader);
         invoker = (InvokerI) allocateInstance(invoker_class);
 
-        transformer_constructor = nothrows_run(() -> transformer.getDeclaredConstructor(
-                MethodType.class, TransformerImpl.class));
+        Class<?> transformer = loadClass(dex, transformer_name, loader);
+        //noinspection unchecked
+        transformer_constructor = (Constructor<MethodHandle>) nothrows_run(() -> transformer
+                .getDeclaredConstructor(MethodType.class, TransformerImpl.class));
+
+        //rename VariadicType to ?
+        //TODO: it is safe?
+        ClassMirror[] vc = arrayCast(ClassMirror.class, VariadicType.class);
+        vc[0].name = "?";
     }
 
-    private static MethodHandle makeTransformer(MethodType type, TransformerImpl impl) {
-        return nothrows_run(() -> transformer_constructor.newInstance(type, impl));
+    //TODO: unsafe get ptypes
+    private static boolean hasVariadicType(MethodType type) {
+        if (type.returnType() == VariadicType.class) {
+            return true;
+        }
+        for (Class<?> arg : type.parameterArray()) {
+            if (arg == VariadicType.class) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static MethodHandle makeTransformer(MethodType type, TransformerF callback) {
-        return makeTransformer(type, regularImpl(callback));
+    public static Class<?> variadicType() {
+        return VariadicType.class;
     }
 
-    private static MethodHandle makeVarargsTransformer(MethodType fixed, TransformerImpl impl) {
+    private static MethodHandle makeTransformer(
+            MethodType fixed, TransformerImpl impl, boolean variadic) {
         MethodHandle out = nothrows_run(() -> transformer_constructor.newInstance(fixed, impl));
-        if (getSdkInt() < 33) {
-            //TODO: improve
+        if (variadic && getSdkInt() < 33) {
+            //TODO: safer way
             MethodHandleMirror[] m = arrayCast(MethodHandleMirror.class, out);
             m[0].handleKind = /*INVOKE_CALLSITE_TRANSFORM*/ 6;
         }
         return out;
     }
 
-    public static MethodHandle makeVarargsTransformer(MethodType fixed, TransformerF callback) {
-        return makeVarargsTransformer(fixed, varargsImpl(callback));
+    public static MethodHandle makeTransformer(
+            MethodType fixed, TransformerF callback, boolean varargs) {
+        if (varargs || hasVariadicType(fixed)) {
+            return makeTransformer(fixed, variadicImpl(fixed, callback, varargs), true);
+        }
+        return makeTransformer(fixed, regularImpl(callback), false);
     }
 
-    public static MethodHandle makeVarargsTransformer(TransformerF callback) {
-        return makeVarargsTransformer(MethodType.methodType(void.class), callback);
+    public static MethodHandle makeTransformer(MethodType type, TransformerF callback) {
+        return makeTransformer(type, callback, false);
+    }
+
+    //Transformer(...)?
+    public static MethodHandle makeVariadicTransformer(TransformerF callback) {
+        return makeTransformer(MethodType.methodType(VariadicType.class), callback, true);
     }
 
     @Keep
@@ -360,16 +391,226 @@ public class Transformers {
         void transform(MethodHandle thiz, EmulatedStackFrame stackFrame) throws Throwable;
     }
 
-    private static String toStringTransformer(MethodType type, boolean isVararg) {
+    private static Class<?> wrapPrimitive(Class<?> prim) {
+        if (prim == int.class) {
+            return Integer.class;
+        }
+        if (prim == long.class) {
+            return Long.class;
+        }
+        if (prim == double.class) {
+            return Double.class;
+        }
+        if (prim == float.class) {
+            return Float.class;
+        }
+        if (prim == char.class) {
+            return Character.class;
+        }
+        if (prim == short.class) {
+            return Short.class;
+        }
+        if (prim == byte.class) {
+            return Byte.class;
+        }
+        if (prim == boolean.class) {
+            return Boolean.class;
+        }
+        throw new IllegalArgumentException(prim + " is not primitive class or void");
+    }
+
+    private static Class<?> unwrapPrimitive(Class<?> wrapper) {
+        if (wrapper == Integer.class) {
+            return int.class;
+        }
+        if (wrapper == Long.class) {
+            return long.class;
+        }
+        if (wrapper == Double.class) {
+            return double.class;
+        }
+        if (wrapper == Float.class) {
+            return float.class;
+        }
+        if (wrapper == Character.class) {
+            return char.class;
+        }
+        if (wrapper == Short.class) {
+            return short.class;
+        }
+        if (wrapper == Byte.class) {
+            return byte.class;
+        }
+        if (wrapper == Boolean.class) {
+            return boolean.class;
+        }
+        return null;
+    }
+
+    private static int getWeight(Class<?> prim) {
+        if (prim == boolean.class) {
+            return 0;
+        }
+        if (prim == byte.class) {
+            return 1;
+        }
+        if (prim == short.class) {
+            return 2;
+        }
+        if (prim == char.class) {
+            return 3;
+        }
+        if (prim == int.class) {
+            return 4;
+        }
+        if (prim == long.class) {
+            return 5;
+        }
+        if (prim == float.class) {
+            return 6;
+        }
+        if (prim == double.class) {
+            return 7;
+        }
+        throw new IllegalArgumentException(prim + " is not primitive class or void");
+    }
+
+    private static boolean canConvertPrimitive(Class<?> from, Class<?> to) {
+        //from & to != void
+        if (from == to) {
+            return true;
+        }
+        if (getWeight(from) > getWeight(to)) {
+            return false;
+        }
+        if (from == boolean.class || to == boolean.class) {
+            return false;
+        }
+        return to != char.class;
+    }
+
+    private static boolean canConvert(Class<?> from, Class<?> to) {
+        if (from == to || from == Object.class || to == Object.class
+                || from == void.class || to == void.class) {
+            return true;
+        }
+        if (from.isPrimitive()) {
+            if (to.isPrimitive()) {
+                return canConvertPrimitive(from, to);
+            }
+            return to.isAssignableFrom(wrapPrimitive(from));
+        } else if (to.isPrimitive()) {
+            if (from.isAssignableFrom(wrapPrimitive(to))) {
+                return true;
+            }
+
+            from = unwrapPrimitive(from);
+            if (from == null) {
+                return false;
+            }
+
+            return canConvertPrimitive(from, to);
+        } else {
+            return true;
+        }
+    }
+
+    private static Class<?> specify(Class<?> fixed, Class<?> required, boolean isArg) {
+        if (fixed == required) {
+            return fixed;
+        }
+        if (fixed == VariadicType.class) {
+            return required;
+        }
+        if (required == VariadicType.class) {
+            if (fixed == void.class || fixed == Object.class) {
+                return fixed;
+            }
+            return null;
+        }
+        if (canConvert(isArg ? required : fixed, isArg ? fixed : required)) {
+            return fixed;
+        }
+        return null;
+    }
+
+    //TODO: insert isVararg to MethodType
+    //TODO: unsafe get rtype/ptypes;
+    private static MethodType specifyNoThrow(MethodType fixed, boolean fixedIsVararg,
+                                             MethodType required, boolean requiredIsVararg) {
+        Objects.requireNonNull(fixed);
+        Objects.requireNonNull(required);
+        if (fixedIsVararg) {
+            if (!requiredIsVararg) {
+                if (required.parameterCount() < fixed.parameterCount()) {
+                    //f = (?, ?, ...)?
+                    //r = (?)?
+                    return null;
+                }
+            }
+        } else {
+            if (requiredIsVararg) {
+                //f = (?)?
+                //r = (?, ...)?
+                return null;
+            } else if (fixed.parameterCount() != required.parameterCount()) {
+                //f = (?)?
+                //r = (?, ?)?
+                return null;
+            }
+        }
+
+        Class<?> rtype = specify(fixed.returnType(), required.returnType(), false);
+        if (rtype == null) {
+            return null;
+        }
+
+        int arg_count = Math.min(fixed.parameterCount(), required.parameterCount());
+        Class<?>[] ptypes = new Class[Math.max(fixed.parameterCount(), required.parameterCount())];
+
+        for (int i = 0; i < arg_count; i++) {
+            ptypes[i] = specify(fixed.parameterType(i), required.parameterType(i), true);
+            if (ptypes[i] == null) {
+                return null;
+            }
+        }
+
+        if (arg_count != ptypes.length) {
+            Class<?>[] tmp = fixed.parameterCount() > arg_count ?
+                    fixed.parameterArray() : required.parameterArray();
+            System.arraycopy(tmp, arg_count, ptypes, arg_count, tmp.length - arg_count);
+        }
+
+        return MethodType.methodType(rtype, ptypes);
+    }
+
+
+    private static MethodType specify(MethodType fixed, boolean fixedIsVararg,
+                                      MethodType required, boolean requiredIsVararg) {
+        MethodType out = specifyNoThrow(fixed, fixedIsVararg, required, requiredIsVararg);
+        if (out == null) {
+            throw newWrongMethodTypeException(fixed, fixedIsVararg, required, requiredIsVararg);
+        }
+        return out;
+    }
+
+    /*//TODO
+    private static EmulatedStackFrame convert(EmulatedStackFrame from,
+                                              MethodType to, boolean toIsVararg) {
+    }*/
+
+
+    //TODO: insert isVararg to MethodType
+    private static String toStringMethodType(MethodType type, boolean isVararg) {
         StringBuilder sb = new StringBuilder();
-        sb.append("Transformer(");
+        sb.append("(");
         for (int i = 0; i < type.parameterCount(); i++) {
             if (i > 0) sb.append(",");
             sb.append(type.parameterType(i).getSimpleName());
         }
         if (isVararg) {
             if (type.parameterCount() > 0) {
-                sb.append(", ");
+                sb.append(',');
             }
             sb.append("...");
         }
@@ -397,11 +638,17 @@ public class Transformers {
 
             @Override
             MethodHandle asType(MethodHandle thiz, MethodType newType) {
+                //TODO: maybe caching?
+                if (hasVariadicType(newType)) {
+                    //TODO
+                    throw new UnsupportedOperationException("Not implemented yet");
+                }
                 return nothrows_run(() -> (MethodHandle) directAsType.invoke(thiz, newType));
             }
 
             @Override
             MethodHandle bindTo(MethodHandle thiz, Object value) {
+                //TODO: bindTo with first primitive parameter
                 return nothrows_run(() -> (MethodHandle) directBindTo.invoke(thiz, value));
             }
 
@@ -413,22 +660,35 @@ public class Transformers {
 
             @Override
             String toString(MethodHandle thiz) {
-                return toStringTransformer(thiz.type(), false);
+                return "Transformer" + toStringMethodType(thiz.type(), false);
             }
         };
     }
 
-    //TODO: checks
-    private static TransformerImpl varargsImpl(TransformerF callback) {
+    private static TransformerImpl variadicImpl(
+            MethodType fixed, TransformerF callback, boolean varargs) {
+
+        final boolean full_variadic = varargs && (fixed.parameterCount() == 0)
+                && (fixed.returnType() == VariadicType.class);
         return new TransformerImpl() {
+
             @Override
             void transform(MethodHandle thiz, EmulatedStackFrame stackFrame) throws Throwable {
-                callback.transform(thiz, stackFrame);
+                if (full_variadic) {
+                    callback.transform(thiz, stackFrame);
+                } else {
+                    if (getSdkInt() <= 32) {
+                        invokeExactWithFrameNoChecks(asType(thiz, stackFrame.type()), stackFrame);
+                    } else {
+                        //TODO: maybe check?
+                        callback.transform(thiz, stackFrame);
+                    }
+                }
             }
 
             @Override
             boolean isVarargsCollector(MethodHandle ignored) {
-                return true;
+                return varargs;
             }
 
             @Override
@@ -437,9 +697,24 @@ public class Transformers {
             }
 
             @Override
-            MethodHandle asType(MethodHandle ignored, MethodType newType) {
+            MethodHandle asType(MethodHandle thiz, MethodType newType) {
                 //TODO: maybe caching?
-                return makeTransformer(newType, callback);
+                if (full_variadic) {
+                    return makeTransformer(newType, callback);
+                } else {
+                    if (thiz.type().equals(newType)) {
+                        if (varargs) {
+                            return makeTransformer(newType, callback);
+                        }
+                        return thiz;
+                    }
+                    MethodType specified = specify(thiz.type(), varargs, newType, false);
+                    if (getSdkInt() >= 33 && !hasVariadicType(newType)) {
+                        return (MethodHandle) nothrows_run(() -> directAsType.invoke(thiz, newType));
+                    }
+                    //TODO
+                    throw new UnsupportedOperationException("Not implemented yet");
+                }
             }
 
             @Override
@@ -450,12 +725,16 @@ public class Transformers {
 
             @Override
             MethodHandle asVarargsCollector(MethodHandle thiz, Class<?> arrayType) {
-                return thiz;
+                if (varargs) {
+                    return thiz;
+                }
+                //TODO
+                throw new UnsupportedOperationException("Not implemented yet");
             }
 
             @Override
             String toString(MethodHandle thiz) {
-                return toStringTransformer(thiz.type(), true);
+                return "Transformer" + toStringMethodType(thiz.type(), true);
             }
         };
     }
@@ -477,14 +756,51 @@ public class Transformers {
         abstract String toString(MethodHandle thiz);
     }
 
-    public static void throwWrongMethodTypeException(MethodType from, MethodType to) {
-        throw new WrongMethodTypeException("Cannot convert " + from + " to " + to);
+    /*
+    //TODO: bind to index
+    //TODO
+    private static class VariadicBindTo implements TransformerF {
+        private final TransformerF delegate;
+        private final Object receiver;
+
+        VariadicBindTo(TransformerF delegate, Object receiver) {
+            this.delegate = delegate;
+            this.receiver = receiver;
+        }
+
+        @Override
+        public void transform(MethodHandle thiz, EmulatedStackFrame stackFrame) throws Throwable {
+            // Create a new emulated stack frame with the full type (including the leading
+            // receiver reference).
+            EmulatedStackFrame stackFrame = EmulatedStackFrame.create(delegate.type());
+
+            // The first reference argument must be the receiver.
+            stackFrame.setReference(0, receiver);
+            // Copy all other arguments.
+            emulatedStackFrame.copyRangeTo(stackFrame, range, 1, 0);
+
+            // Perform the invoke.
+            invokeFromTransform(delegate, stackFrame);
+            stackFrame.copyReturnValueTo(emulatedStackFrame);
+
+        }
+    }*/
+
+    private static WrongMethodTypeException newWrongMethodTypeException(MethodType from, MethodType to) {
+        return new WrongMethodTypeException("Cannot convert " + from + " to " + to);
+    }
+
+    private static WrongMethodTypeException newWrongMethodTypeException(
+            MethodType from, boolean fromIsVararg, MethodType to, boolean toIsVararg) {
+        return new WrongMethodTypeException(
+                "Cannot convert " + toStringMethodType(from, fromIsVararg)
+                        + " to " + toStringMethodType(to, toIsVararg));
     }
 
     public static void invokeExactWithFrameNoChecks(
             MethodHandle target, EmulatedStackFrame stackFrame) throws Throwable {
         if (invoke_transformer.isInstance(target)) {
-            //TODO: android 8-12L nominalType to type
+            //FIXME: android 8-12L convert nominalType to type
             invoker.transform(target, stackFrame.esf);
         } else {
             invoker.invokeExactWithFrame(target, stackFrame.esf);
@@ -494,7 +810,7 @@ public class Transformers {
     public static void invokeExactWithFrame(
             MethodHandle target, EmulatedStackFrame stackFrame) throws Throwable {
         if (!target.type().equals(stackFrame.type())) {
-            throwWrongMethodTypeException(stackFrame.type(), target.type());
+            throw newWrongMethodTypeException(stackFrame.type(), target.type());
         }
         invokeExactWithFrameNoChecks(target, stackFrame);
     }
